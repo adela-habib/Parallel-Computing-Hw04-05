1e3d1a4b4ddbcd77d0e453b0d75225ff157a00c5
GGriva <grivag@rpi.edu>
2018-04-20 13:17:15 -0400

    Fixed a whole bunch of stuff.

--- assignment4-5.c
+++ assignment4-5.c
@@ -25,7 +25,7 @@
 #define DEAD  0
 
 #define u_size 1024 //world size 1024x1024
-#define num_ticks 128 //128 ticks
+#define num_ticks 3 //128 ticks --> CHANGE BACK
 
 /***************************************************************************/
 /* Global Vars *************************************************************/
@@ -66,33 +66,31 @@ int main(int argc, char *argv[])
     MPI_Comm_size( MPI_COMM_WORLD, &mpi_commsize);
     MPI_Comm_rank( MPI_COMM_WORLD, &mpi_myrank);
     
-// Init 16,384 RNG streams - each rank has an independent stream
+    // Init 16,384 RNG streams - each rank has an independent stream
     InitDefault();
     
-// Note, used the mpi_myrank to select which RNG stream to use.
-// You must replace mpi_myrank with the right row being used.
-// This just show you how to call the RNG.    
+    // Note, used the mpi_myrank to select which RNG stream to use.
+    // You must replace mpi_myrank with the right row being used.
+    // This just show you how to call the RNG.    
     printf("Rank %d of %d has been started and a first Random Value of %lf\n", 
        mpi_myrank, mpi_commsize, GenVal(mpi_myrank));
     
     MPI_Barrier( MPI_COMM_WORLD );
     
-// Insert your code
-
-//Get number of pthreads and threshold value
+    //Get number of pthreads and threshold value
     sscanf(argv[1], "%d", &num_pthreads); //number of pthreads
     sscanf(argv[2], "%lf", &threshold); //threshold value
     
-//Start time of program
+    //Start time of program
     if (mpi_myrank == 0)
     {
         start_time = MPI_Wtime();
     }   
     
-//determine number of rows for each rank and each thread
+    //determine number of rows for each rank and each thread
     rows_per_rank = u_size/mpi_commsize; //number of rows per rank
     
-//allocate space for rows and columns on each rank
+    //allocate space for rows and columns on each rank
     my_rows = (int **)calloc(rows_per_rank, sizeof(int*));
     
    for (int i=0; i<rows_per_rank; i++)
@@ -104,7 +102,7 @@ int main(int argc, char *argv[])
    top_ghost_row = (int *)malloc(sizeof(int)*u_size);
    bottom_ghost_row = (int *)malloc(sizeof(int)*u_size);
     
-//Randomly initialize universe
+   //Randomly initialize universe
     for (int i=0; i<rows_per_rank; i++)
     {
         for (int j=0; j < u_size; j++)
@@ -122,53 +120,53 @@ int main(int argc, char *argv[])
 
     if (mpi_myrank == 0){ printf("Done Making Universe.\n"); }
     
-//Create Pthreads
+    //Create Pthreads
     pthread_t p_threads[num_pthreads-1];
     pthread_attr_t attr;
     pthread_attr_init (&attr);
   
     pthread_id = 0;
-//1 or more pthreads
-    if (num_pthreads > 0)
+    //2 or more pthreads
+    if (num_pthreads > 1)
     {
       rows_per_thread = rows_per_rank/num_pthreads; //number of rows per thread
       
       //Create barrier to sync ticks in pthreads
       pthread_barrier_init(&pbarrier, NULL, num_pthreads);
       
-      //Run the simulation on calling thread (pthread 0)
-      int arg = 0;
-      run_simulation(&arg);
+      if (mpi_myrank ==0)
+	{
+	  printf("Past run_simulation(0). \n");
+	}
 
-      if (mpi_myrank ==0) { printf("Past run_simulation(0). \n"); }
-
-      //pthreads 1 to num_pthreads run simulation function
+      pthread_id = 1;
+      //pthreads 1 to num_pthreads create and run simulation function
       for(int i=1; i < num_pthreads; i++)
       {       
-        //create pthreads
         pthread_create(&p_threads[i-1], &attr, run_simulation, (void *) &pthread_id);
-
         pthread_id++;       
       }
 
-      if (mpi_myrank ==0){printf("Done Making Pthreads.\n"); }
+      if (mpi_myrank ==0)
+	{
+	  printf("Done Making Pthreads.\n");
+	}
 
-      //join threads to make sure all pthreads in rank are done
-      for(int i = 0; i < num_pthreads - 1; i++)
+    }
+    else
       {
-              pthread_join(p_threads[i], NULL);
+	rows_per_thread = rows_per_rank;
       }
 
-    }
-//No pthreads
-    else
-    {
-        rows_per_thread = rows_per_rank;
+    int arg = 0;
+    run_simulation(&arg);     
 
-    	int arg = 0;
-        run_simulation(&arg);     
-    }
-      
+    //join threads to make sure all pthreads in rank are done
+    for(int i = 0; i < num_pthreads - 1; i++)
+      {
+	pthread_join(p_threads[i], NULL);
+      }
+
     MPI_Barrier(MPI_COMM_WORLD);
 
     //End time of program
@@ -180,7 +178,7 @@ int main(int argc, char *argv[])
     //End MPI
     MPI_Finalize(); 
  
-//Free allocated memory
+    //Free allocated memory
     free(top_ghost_row);
     free(bottom_ghost_row);
     for(int i=0; i < rows_per_rank; i++)
@@ -199,167 +197,210 @@ int main(int argc, char *argv[])
 //runs a simulation for a tick for given rows
 int** sim_tick(int starting_row)
 {
-    int curr_row = starting_row;
-    //array to hold temporary table
-    int ** temp_table = (int**)calloc(rows_per_thread, sizeof(int *));
+  if(mpi_myrank==0){printf("INSIDE SIM_TICK.\n");}
+  
+  int curr_row = starting_row;
+  //array to hold temporary table
+  int ** temp_table = (int**)calloc(rows_per_thread, sizeof(int *));
     
-    //For each row in the table
-    for(int i = 0; i < rows_per_thread; i++)
+  //For each row in the table
+  for(int i = 0; i < rows_per_thread; i++)
     {    
-        //array to hold temp results
-        int * temp_cells = (int *)calloc(u_size, sizeof(int));
-        
-        //for each column in a row
-        for(int curr_col = 0; curr_col < u_size; curr_col++)
+      //array to hold temp results
+      int * temp_cells = (int *)calloc(u_size, sizeof(int));
+
+      // if(mpi_myrank==0){printf("Allocated space for temp_cells.\n");}
+      
+      //for each column in a row
+      for(int curr_col = 0; curr_col < u_size; curr_col++)
         {
-            int num_alive_neighbors = 0; //keep count of live neighbors  
-            //set direction
-            int left = (curr_col-1) % u_size;
-            int right = (curr_col + 1) % u_size;
-            int up = i+1;
-            int down = i-1;
+	  int num_alive_neighbors = 0; //keep count of live neighbors  
+	  //set direction
+	  int left = (curr_col - 1 + u_size) % u_size;
+	  int right = (curr_col + 1) % u_size;
+	  int up = i-1;
+	  int down = i+1;
         
-            //count number of alive neighbors
-            if (curr_row == 0) //top thread row - use top ghost row
-            { 
-                num_alive_neighbors = my_rows[curr_row][left] + my_rows[curr_row][right] + top_ghost_row[curr_col] + my_rows[down][curr_col] +
-                  top_ghost_row[left] + top_ghost_row[right] + my_rows[down][left] + my_rows[down][right];         
-            }
-            else if (curr_row == starting_row + rows_per_thread-1) //bottom thread row - use bottom ghost row
+	  //count number of alive neighbors
+	  if (curr_row == 0) //top thread row - use top ghost row
             {
-                num_alive_neighbors = my_rows[curr_row][left] + my_rows[curr_row][right] + my_rows[up][curr_col] + bottom_ghost_row[curr_col] +
-                  my_rows[up][left] + my_rows[up][right]+ bottom_ghost_row[left] + bottom_ghost_row[right];
+	      //if(mpi_myrank==0){printf("I am in the top row.\n");}
+
+	      /*
+	      if(mpi_myrank==0)
+		{
+		  printf("cell to the left:  %d\n", my_rows[curr_row][left]);
+		  printf("cell to the right: %d\n", my_rows[curr_row][right]);
+		  printf("cell above:        %d\n", top_ghost_row[curr_col]);
+		  printf("cell below:        %d\n", my_rows[down][curr_col]);
+		  printf("cell up and left:  %d\n", top_ghost_row[left]);
+		  printf("cell up and right: %d\n", top_ghost_row[right]);
+		  printf("cell down left:    %d\n", my_rows[down][left]);
+		  printf("cell down right:   %d\n", my_rows[down][right]);
+		}
+	      */
+	      
+	      num_alive_neighbors = my_rows[curr_row][left] + my_rows[curr_row][right] + top_ghost_row[curr_col] + my_rows[down][curr_col] +
+		top_ghost_row[left] + top_ghost_row[right] + my_rows[down][left] + my_rows[down][right];
+
+	      //if(mpi_myrank==0){printf("I finished counting neighbors for top row.\n");}
             }
-            else //middle thread row
+	  else if (curr_row == starting_row + rows_per_thread-1) //bottom thread row - use bottom ghost row
             {
-                num_alive_neighbors = my_rows[curr_row][left] + my_rows[curr_row][right] + my_rows[up][curr_col] + my_rows[down][curr_col] +
-                  my_rows[up][left] + my_rows[up][right] + my_rows[down][left] + my_rows[down][right];
+	      num_alive_neighbors = my_rows[curr_row][left] + my_rows[curr_row][right] + my_rows[up][curr_col] + bottom_ghost_row[curr_col] +
+		my_rows[up][left] + my_rows[up][right]+ bottom_ghost_row[left] + bottom_ghost_row[right];
+            }
+	  else //middle thread row
+            {
+	      num_alive_neighbors = my_rows[curr_row][left] + my_rows[curr_row][right] + my_rows[up][curr_col] + my_rows[down][curr_col] +
+		my_rows[up][left] + my_rows[up][right] + my_rows[down][left] + my_rows[down][right];
             }
 
-            //(ii.) Read all the statuses of the neighbors depending upon the number of live/dead neighbors for the current tick and set the appropiate live/dead count.
-            //ADDITIONAL RANDOMNESS
-            //if random value is greater than threshold - play the game
-            if (GenVal(mpi_myrank*curr_row+curr_col) > threshold)
+	  //if(mpi_myrank==0){printf("Finished counting live neighbors.\n");}
+
+	  //(ii.) Read all the statuses of the neighbors depending upon the number of live/dead neighbors for the current tick
+	  //and set the appropiate live/dead count.
+	  //ADDITIONAL RANDOMNESS
+	  //if random value is greater than threshold - play the game
+	  if (GenVal(mpi_myrank*curr_row+curr_col) > threshold)
             {
-                //1- Any live cell with fewer than two live neighbors dies, as if caused by under-population.
-                if (num_alive_neighbors < 2)
+	      //1- Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+	      if (num_alive_neighbors < 2)
                 {
                   temp_cells[curr_col] = DEAD; 
                 }
-                //2- Any live cell with two or three live neighbors lives on to the next generation
-                //4- Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
-                else if(num_alive_neighbors < 4)
+	      //2- Any live cell with two or three live neighbors lives on to the next generation
+	      //4- Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
+	      else if(num_alive_neighbors < 4)
                 {
-                    if((num_alive_neighbors == 3) & (my_rows[curr_row][curr_col]==DEAD))
+		  if((num_alive_neighbors == 3) & (my_rows[curr_row][curr_col]==DEAD))
                     {
                       temp_cells[curr_col] = ALIVE; // by reproduction
                     }
-                    else
+		  else
                     {
                       temp_cells[curr_col] = my_rows[curr_row][curr_col]; //stays the same
                     }
                 }
-                //3- Any live cell with more than three live neighbors dies, as if by over-population
-                else if(num_alive_neighbors > 3)
+	      //3- Any live cell with more than three live neighbors dies, as if by over-population
+	      else if(num_alive_neighbors > 3)
                 {
-                    temp_cells[curr_col] = DEAD;
+		  temp_cells[curr_col] = DEAD;
                 }
             }
-            //else, randomly select alive or dead
-            else
+	  //else, randomly select alive or dead
+	  else
             {
-                if (GenVal(mpi_myrank*curr_row+curr_col) > 0.5 )
+	      if (GenVal(mpi_myrank*curr_row+curr_col) > 0.5 )
                 { 
-                    temp_cells[curr_col] = ALIVE; //if random number if even, it is alive
+		  temp_cells[curr_col] = ALIVE; //if random number if even, it is alive
                 }
-                else
+	      else
                 {
-                   temp_cells[curr_col] = DEAD; //random number is odd, it is dead
+		  temp_cells[curr_col] = DEAD; //random number is odd, it is dead
                 }
             }
         } 
-    
-        //set the temporary table
-        temp_table[i] = temp_cells;
       
-        //increment the current row
-        curr_row++;  
+      //set the temporary table
+      temp_table[i] = temp_cells;
+      
+      //increment the current row
+      curr_row++;  
     } 
-    return temp_table;
+  return temp_table;
 } 
 
 
 //Funtion ran in each pthread
 void *run_simulation(void *void_pthread_id_ptr)
 {
-    int *pthread_id_ptr = (int *)void_pthread_id_ptr;
-    int pthread_id = *pthread_id_ptr;
-   
-    //play the game
-    for(int t = 0; t < num_ticks; t++)
-    {                              
-        //(i.) Exchange row data with different MPI ranks for the ghost rows (the rows shared between MPI ranks). You exchange this (ghost) row data from thread 0 of each MPI rank. 
-        //Be careful here on how to use mutexes so as to avoid deadlocks. Also note that this row exchange uses alive/dead status data from the previous tick. 
+  int *pthread_id_ptr = (int *)void_pthread_id_ptr;
+  int pthread_id = *pthread_id_ptr;
+
+  printf("Rank %d: Thread id %d is created and running\n", mpi_myrank, pthread_id );
+
+  //play the game
+  for(int t = 0; t < num_ticks; t++)
+    {
+      if(mpi_myrank == 0){printf("TICK %d\n", t);}
+      
+      //(i.) Exchange row data with different MPI ranks for the ghost rows (the rows shared between MPI ranks).
       if (pthread_id == 0)
         {
-            pthread_id0();
+	  pthread_id0();
         }
-        MPI_Barrier(MPI_COMM_WORLD);
-	
-	if (mpi_myrank ==0){printf("Finished pthread_id0() function.\n\n"); }
-	
-        //If there are 0 or 1 pthreads
-        if ( (num_pthreads == 0) | (num_pthreads == 1))
-        {
-            int** temp_table = sim_tick(0);
-          
-            for (int i=0; i<rows_per_thread; i++)
-            {
-                for (int j=0; j<u_size; j++)
-                {
-                    my_rows[i][j] = temp_table[i][j];
-                }
-            }
-          
-            //free temp table
-            for(int i=0; i < rows_per_thread; i++)
-            {
-                  free(temp_table[i]);
-            }
-            free(temp_table);
-        }
-        //multiple pthreads
-        else
-        {
-            //(ii.) Read all the statuses of the neighbors depending upon the number of live/dead neighbors for the current tick and set the appropiate live/dead count.        
-            int starting_row = pthread_id * rows_per_thread;
-            int** temp_table = sim_tick(starting_row);
 
-	    if(mpi_myrank ==0){printf("Finished sim_tick.\n\n");}
-	    
-            //(iii.) Finally, update the cell status
-            for (int i=starting_row; i<rows_per_thread; i++)
-            {
-                for (int j=0; j<u_size; j++)
-                {
-                    my_rows[i][j] = temp_table[i][j];
-                }
-            }
-
-            //Free the temp table
-            for(int i = 0; i < rows_per_thread; i++)
-            {
-                free(temp_table[i]);
-            }
-            free(temp_table);
-        }
+      if (mpi_myrank ==0){printf("Finished pthread_id0() function.\n\n"); }
       
-        //Use pthread barrier to make sure each thread finishes a tick before moving on
-        if(num_pthreads > 0) 
-        {
-            pthread_barrier_wait(&pbarrier);
-        }
+      //If there are 0 or 1 pthreads
+      if ( (num_pthreads == 0) | (num_pthreads == 1) )
+	{
+	      
+	  if (mpi_myrank==0){printf("There are only 0 or 1 threads.\n");}
+	  
+	  int** temp_table = sim_tick(0);
+	  
+	  for (int i=0; i<rows_per_thread; i++)
+	    {
+	      for (int j=0; j<u_size; j++)
+		{
+		  my_rows[i][j] = temp_table[i][j];
+		}
+	    }
+	  
+	  //free temp table
+	  for(int i=0; i < rows_per_thread; i++)
+	    {
+	      free(temp_table[i]);
+	    }
+	  free(temp_table);
+	}
+      //multiple pthreads
+      else 
+	{
+	  if(mpi_myrank==0){printf("There are multiple threads. Inside run_sim for threads > 0\n");}
+	  if(mpi_myrank==0){printf("TESTING HERE. PTHREADID = %d\n", pthread_id);}
+	  
+	  //(ii.) Read all the statuses of the neighbors depending upon the number of live/dead neighbors for the current tick
+	  //and set the appropiate live/dead count.        
+	  int starting_row = pthread_id * rows_per_thread;
+	  int** temp_table = sim_tick(starting_row);
+	  
+	  if(mpi_myrank ==0){printf("Finished sim_tick.\n\n");}
+	  
+	  //(iii.) Finally, update the cell status
+	  for (int i=starting_row; i<rows_per_thread; i++)
+	    {
+	      for (int j=0; j<u_size; j++)
+		{
+		  my_rows[i][j] = temp_table[i][j];
+		}
+	    }
+	  if(mpi_myrank==0){printf("Updated universe\n");}
+	  
+	  //Free the temp table
+	  for(int i = 0; i < rows_per_thread; i++)
+	    {
+	      free(temp_table[i]);
+	    }
+	  free(temp_table);
+	}
+      if(mpi_myrank==0){printf("Freed temporary universe.\n");}
+      
+      //Use pthread barrier to make sure each thread finishes a tick before moving on
+      if(num_pthreads > 1) 
+	{
+	  if(mpi_myrank==0){printf("BEFORE CALLING PTHREAD BARRIER.\n");}
+	  pthread_barrier_wait(&pbarrier);
+	}
+      if(mpi_myrank==0){printf("Finished pthread barrier.\n");}
+
+      if(mpi_myrank==0){printf("FINISHED 1 TICK.\n");}
+
+      
     }
+    
     return NULL;
 }
 
