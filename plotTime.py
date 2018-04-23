ed3e4c075040bab6678ae3c487ba5c66cb84955e
habiba1396 <adelahanihabib@gmail.com>
2018-04-23 16:26:28 -0400

    Add files via upload

--- plotTime.py
+++ plotTime.py
@@ -3,41 +3,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-time = np.loadtxt("Time.txt", usecols = (0, 1, 2, 3), delimiter=', ')
-print time.shape
 
-plt.figure()
+IOtime = np.loadtxt("IOtime.txt")
+time = np.loadtxt("Time.txt", delimiter =', ')
+lab = [0, 4, 8, 32, 64]
 
-k = 0
+ranks = IOtime[:, 0]
+threads = IOtime[:, 1]
+exct=IOtime[:, 2]
+
+plt.figure(3)
+plt.plot(np.log2(ranks), exct, 'o-')
+plt.xlabel("Number of ranks")
+plt.ylabel("Execution time [s]")
+plt.xticks([6,7,8,9,10,11,12,13,14])
+
+plt.figure(1)
+time = np.loadtxt("Time.txt", delimiter =', ')
+c = ["red", "orange", "green", "blue"]
 for i in range(4):
 	x = []
 	y = []
-	for i in range(4):
-		x.append(time[i+k,1]*time[i+k,2])
-		y.append(time[i+k, 3])
-	
-	k += 4
-	plt.plot(x, y, 'o')
+	k = i
+	for j in range(4):
+		x.append(time[k, 1]*time[k, 2])
+		y.append(time[k, 3])
+        	k += 5
+	plt.plot(np.log2(x),y, 'o-', color = c[i], label = str(lab[i]) +"-pthreads")
+plt.legend(frameon=False)
+plt.xlabel(r"log$_{2}$(ranks*pthreads)")
+plt.ylabel("execution time [s]")
+plt.xticks([7,8,9,10,11,12,13,14])
+
+plt.figure(2)
+c = ["red", "orange", "green", "blue"]
+
+for i in range(4):
+        x = []
+        y = []
+        k = i+5  
+        for j in range(3):
+                print k, time[k, 1],time[i, 3]
+                x.append(time[k, 1]*time[k, 2])
+                y.append(time[i, 3]/time[k, 3])
+                k += 5
+        plt.plot(np.log2(x),y, 'o-', color = c[i], label = str(lab[i]) +"-pthreads")
+plt.legend(loc = "upper left", frameon=False)
+plt.xlabel(r"log$_{2}$(ranks*pthreads)")
+plt.ylabel("relative speedUp")
+plt.xticks([9,10,11,12,13,14])
 
 plt.show()
 exit(0)
-
-
-
-time = np.reshape(time, (4, 20))
-print time
-c = ["red", "orange", "green", "blue"]
-k = 3
-for i in range(4):
-	x = []
-	y = []
-	for j in range(4):
-		print  k, time[j, k-2],time[j, k-1], time[j,k]   
-		x.append(time[j, k-1]*time[j, k-2])
-		y.append(time[j,k])
-        plt.plot(x, y, 'o', color = c[i], label = str(int(time[j, k-1])) +"-pthreads")
-	k = k+3+1
-plt.legend()
-plt.show()
-
 
